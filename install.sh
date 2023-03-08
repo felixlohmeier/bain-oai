ce5133b29811eb88e381537e3141111c3fd9bbe7
Felix Lohmeier <mail@felixlohmeier.de>
2023-03-08 16:14:23 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-sudo apt update && sudo apt install composer php php-xml
+export DEBIAN_FRONTEND=noninteractive
+sudo apt-get update && sudo apt-get -y install composer php php-xml
 wget https://github.com/vufind-org/vufindharvest/archive/refs/tags/v5.1.0.zip
 unzip v5.1.0.zip
 cd vufindharvest-5.1.0
